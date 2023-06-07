a4fba71fc922bc3e5a503fc0d730d420979a74ac
aimeariadne <aime@multiplier.com.br>
2023-06-07 16:05:48 +0100

    create notes creation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent
+  },
+  // {
+  //   path: 'listar-pensamento',
+  //   component: ListarPensamentoComponent
+  // },
+  // {
+  //   path: 'pensamentos/editar-pensamento/:id',
+  //   component: EditarPensamentoComponent
+  // },
+  // {
+  //   path: 'pensamentos/excluir-pensamento/:id',
+  //   component: ExcluirPensamentoComponent
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'listar-pensamento',
+  //   pathMatch: 'full'
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
